0c229281727e57b3a50b2dd45e3bb9790c6feb47
robb <ssekabirarobert037@gmail.com>
2024-08-04 09:52:26 -0700

    initial commit

---
+++ frontend/public/types/404.ts
@@ -0,0 +1,19 @@
+interface structure {
+    reload(button: HTMLButtonElement): any,
+}
+
+class pageReload implements Required<Pick<structure, 'reload'>> {
+    public static instance: any = new pageReload().reload();
+    private constructor(){}
+
+    reload(button: HTMLButtonElement = document.querySelector('button') as HTMLButtonElement) {
+       if(!(button === null)){
+            button.addEventListener('click', (event: any) => {
+                window.setTimeout(() => window.location.reload(), 10 as number)
+            });
+            return;   
+       } else return;
+    }
+}
+
+pageReload.instance
